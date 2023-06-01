b8163e56a1fb96602c6852c3d8067e5d4fa66932
iW90 <ingrid.wagner.dev@gmail.com>
2023-05-31 21:09:15 -0400

    feat: minishell.h added

---
+++ incl/minishell.h
@@ -0,0 +1,22 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <curses.h>
+# include <termios.h>
+# include <termcap.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+# include "../libs/libft/incl/libft.h"
+
+#endif
