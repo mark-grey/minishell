e9ac67826744b708651b37595a7bd2f5cf90f902
iW90 <ingrid.wagner.dev@gmail.com>
2023-06-09 21:04:02 -0400

    feat: add prompt initial configuration

--- srcs/checkers.c
+++ srcs/checkers.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 21:32:31 by maalexan          #+#    #+#             */
-/*   Updated: 2023/06/09 21:03:05 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/06/09 21:03:58 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	is_builtin(char *str)
 {
-	if (!ft_strncmp(str, "cleiton\0", 8))
+	if (!ft_strncmp(str, "ping\0", 8))
 	{
-		printf("teste\n");
+		printf("pong\n");
 		return (1);
 	}
 	if (!ft_strncmp(str, "exit\0", 5))
