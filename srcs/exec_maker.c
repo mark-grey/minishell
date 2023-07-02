38cf9fcfbb72e4ad6dc42b99ef558eb05ab140fe
Marcos Alexandre Chiu Mariano Da Silva <maalexan@c1r6p1.42sp.org.br>
2023-07-02 15:41:34 -0300

    feat: debug quote part

--- srcs/exec_maker.c
+++ srcs/exec_maker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_maker.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 16:18:47 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/01 20:43:57 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/02 15:40:21 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ static int	count_args(char *args, int single_arg)
 	int	count;
 
 	i = 0;
-	count = 1;
+	count = 0;
 	while (args[i])
 	{
 		if (args[i] == ' ' || !args[i + 1])
 		{
 			if (single_arg)
-				return (i + 1);
+				{printf("I'm gonna leave at index args[%i], char: %i\n", i, (int)args[i]);return (i);}
 			else
 				count++;
 		}
@@ -55,10 +55,13 @@ static char	*set_arg(char *args, char **pointer)
 	printf("gonna count from %s\n", &args[new_arg]);
 	len = count_args(&args[new_arg], 1);
 	(void)pointer;
-	printf("len is %i\n\n", len);
-	new_arg += len;
+	new_arg += len + 1;
 	if (!args[new_arg])
+	{
+		len++;
 		new_arg = 0;
+	}
+	printf("len is %i\n", len);
 	return NULL;
 }
 
@@ -70,7 +73,7 @@ char	**stringify_args(char *args)
 
 	i = 0;
 	count = count_args(args, 0);
-	printf("I counted %i args\n", count);
+	printf("\n\nI counted %i args\n", count);
 	pointers = malloc(sizeof(char *) * (count + 1));
 	if (!pointers)
 		exit_program(OUT_OF_MEMORY);
@@ -86,7 +89,9 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (-1);
 	stringify_args(argv[1]);
-	print_args(argv);
+	if (argv[2])
+		stringify_args(argv[2]);
+	print_args(argv + 1);
 }
 
 //	gcc -Wall -Werror -Wextra -I ./incl ./srcs/exec_maker.c ./srcs/builtin_caller.c ./srcs/cleaner.c ./srcs/env_parser.c ./srcs/env_utils.c ./srcs/exec_finder.c ./srcs/input_parser_gets.c ./srcs/input_parser.c ./srcs/input_utils.c  ./libs/libft/libft.a -o execs 
