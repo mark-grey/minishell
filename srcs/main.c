90f8f267da5699f306c9f301b0c33e637bd29b54
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-09 20:33:19 -0300

    test: transfer tests to another file

--- srcs/main.c
+++ srcs/main.c
@@ -6,35 +6,12 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 20:37:15 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/09 20:09:40 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/09 20:32:33 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	print_args(char **args)
-{
-	int	i;
-
-	i = 1;
-	if (!args)
-		return ;
-	while (*args)
-		printf("[%i]%s\t", i++, *args++);
-}
-
-static void	print_cli_list(t_cli *list)
-{
-	if (!list)
-		return ;
-	printf("Cmd: %s \t//\t", list->cmd);
-	printf("Args: ");
-	print_args(list->args);
-	printf(" \t//\tDirector: %s\n", list->director);
-	print_cli_list(list->next);
-}
-/*
-*/
 void	prompt_user(const char *prompt, t_env *env_list)
 {
 	char	*line;
@@ -43,18 +20,14 @@ void	prompt_user(const char *prompt, t_env *env_list)
 	char	*path;
 
 	path = get_var_value("PATH", env_list);
-
 	line = readline(prompt);
 	if (!line)
 		exit_program(127);
 	if (!bar_input(line))
 	{
-		printf("Line: %s\n", line);
 		cmds = parse_input(line, path);
-
 		if (cmds)
-			print_cli_list(cmds);
-			//call_builtin(cmds->cmd, cmds->args, env_list);
+			call_builtin(cmds->cmd, cmds->args, env_list);
 
 		ctrl = get_control();
 		if (ctrl->cli)
