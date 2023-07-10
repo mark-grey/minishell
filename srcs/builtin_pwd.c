1b64ef05f84640d47d33851de8984d18c20b510e
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-10 19:19:08 -0300

    fix: add newline at the end to the pwd builtin

--- srcs/builtin_pwd.c
+++ srcs/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 18:12:40 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/08 15:44:51 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/10 19:06:59 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ void	b_pwd(void)
 
 	if (!getcwd(current_path, sizeof(current_path)))
 		exit_program(-1);
-	printf("%s", current_path);
+	printf("%s\n", current_path);
 }
