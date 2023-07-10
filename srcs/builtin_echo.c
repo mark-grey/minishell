bfc0aef4205737349d78437e2d64f2799656090a
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-10 19:00:45 -0300

    fix: add space between echo args

--- srcs/builtin_echo.c
+++ srcs/builtin_echo.c
@@ -6,11 +6,21 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 20:57:15 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/08 20:23:34 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/10 18:56:21 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	print_args (char **args)
+{
+	while (*args)
+	{
+		printf("%s", *args++);
+		if (*args)
+			printf(" ");
+	}
+}
 
 void	b_echo(char **args)
 {
@@ -18,14 +28,12 @@ void	b_echo(char **args)
 		return ;
 	if (ft_strncmp(*args, "-n\0", 3))
 	{
-		while (*args)
-			printf("%s", *args++);
+		print_args(args);
 		printf("\n");
 	}
 	else
 	{
 		args++;
-		while (*args)
-			printf("%s", *args++);
+		print_args(args);
 	}
 }
