4d1e8bf74c6cd48a683065924e5329235a2dd1bf
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-10 21:04:09 -0300

    fix: show underline variable at the end of env builtin

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 20:25:59 by inwagner          #+#    #+#             */
-/*   Updated: 2023/06/28 22:11:34 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/10 20:58:52 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 void	b_env(t_env *list)
 {
+	t_env	*head;
+
+	head = list;
+	list = list->next;
 	while (list)
 	{
 		printf("%s=%s\n", list->key, list->value);
 		list = list->next;
 	}
+	printf("%s=%s\n", head->key, head->value);
 }
