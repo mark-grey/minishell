036a481c51a3af6fa1fd6a946e72fda22585ca5e
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-11 21:12:51 -0300

    refactor: remove null at the end of comparison

--- srcs/builtin_caller.c
+++ srcs/builtin_caller.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 19:34:20 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/10 20:22:34 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/11 20:06:31 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ void	call_builtin(char *builtin, char **args, t_env *env)
 {
 	if (!builtin)
 		return ;
-	if (!ft_strncmp(builtin, "ping\0", 5))
+	if (!ft_strncmp(builtin, "ping", 5))
 		printf("pong\n");
-	else if (!ft_strncmp(builtin, "cd\0", 3))
+	else if (!ft_strncmp(builtin, "cd", 3))
 		b_cd(args, env);
-	else if (!ft_strncmp(builtin, "pwd\0", 4))
+	else if (!ft_strncmp(builtin, "pwd", 4))
 		b_pwd();
-	else if (!ft_strncmp(builtin, "echo\0", 5))
+	else if (!ft_strncmp(builtin, "echo", 5))
 		b_echo(args);
-	else if (!ft_strncmp(builtin, "unset\0", 6))
+	else if (!ft_strncmp(builtin, "unset", 6))
 		b_unset(env, args);
-	else if (!ft_strncmp(builtin, "export\0", 7))
+	else if (!ft_strncmp(builtin, "export", 7))
 		b_export(env, args);
-	else if (!ft_strncmp(builtin, "env\0", 4))
+	else if (!ft_strncmp(builtin, "env", 4))
 		b_env(env);
-	else if (!ft_strncmp(builtin, "exit\0", 5))
+	else if (!ft_strncmp(builtin, "exit", 5))
 		exit_program(0);
 }
