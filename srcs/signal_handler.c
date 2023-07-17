a33035f1cd304809f0c3dc79e61fe5ed50f33482
Marcos <marcos.chiu@gmail.com>
2023-07-17 16:33:12 -0300

    feat: signal handled

--- srcs/signal_handler.c
+++ srcs/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 14:49:23 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/17 16:14:13 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:32:25 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,21 @@ static void sig_handler(int sig)
 	rl_redisplay(); // Redisplay o prompt
 }
 
+/* MODO NÃO INTERATIVO
+Usado quando o minishell está no modo não-interativo,
+o que significa que não está aguardando a entrada do
+usuário. Por exemplo, quando um comando está em execução
+(ou seja, cat), o minishell não deve reagir a SIGINT e
+SIGQUIT porque apenas o processo em execução (cat)
+precisa reagir a esses sinais.
+*/
+
+static void	non_interactive_mode(int sig)
+{
+	(void)sig;
+	write (1, "\n", 1);
+}
+
 void	set_signals(int mode)
 {
 	if (mode == 0)
@@ -39,16 +54,8 @@ void	set_signals(int mode)
 	}
 	if (mode == 2)
 	{
-		signal(SIGINT, SIG_IGN);
-		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, non_interactive_mode);
+		signal(SIGQUIT, non_interactive_mode);
 	}
 }
 
-/* MODO NÃO INTERATIVO
-Usado quando o minishell está no modo não-interativo,
-o que significa que não está aguardando a entrada do
-usuário. Por exemplo, quando um comando está em execução
-(ou seja, cat), o minishell não deve reagir a SIGINT e
-SIGQUIT porque apenas o processo em execução (cat)
-precisa reagir a esses sinais.
-*/
