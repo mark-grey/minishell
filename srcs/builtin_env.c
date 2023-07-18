5bdc27a9850fb2e4edf7ad2f5af71d370394a8f5
iW90 <ingrid.wagner.dev@gmail.com>
2023-07-18 14:52:01 -0300

    refactor: improve env builtin

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -6,11 +6,19 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 20:25:59 by inwagner          #+#    #+#             */
-/*   Updated: 2023/07/17 19:51:21 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:50:35 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	print_env(char *key, char *value)
+{
+	ft_putstr_fd(key, STDOUT_FILENO);
+	ft_putstr_fd("=", STDOUT_FILENO);
+	ft_putstr_fd(value, STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+}
 
 int	b_env(char **path, t_env *list)
 {
@@ -22,15 +30,9 @@ int	b_env(char **path, t_env *list)
 	list = list->next;
 	while (list)
 	{
-		ft_putstr_fd(list->key, STDOUT_FILENO);
-		ft_putstr_fd("=", STDOUT_FILENO);
-		ft_putstr_fd(list->value, STDOUT_FILENO);
-		ft_putstr_fd("\n", STDOUT_FILENO);
+		print_env(list->key, list->value);
 		list = list->next;
 	}
-	ft_putstr_fd(head->key, STDOUT_FILENO);
-	ft_putstr_fd("=", STDOUT_FILENO);
-	ft_putstr_fd(head->value, STDOUT_FILENO);
-	ft_putstr_fd("\n", STDOUT_FILENO);
+	print_env(head->key, head->value);
 	return (0);
 }
